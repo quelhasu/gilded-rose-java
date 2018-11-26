f207c91ec469ba2b3b99780d5a0241c84c87d61b
Unknown <uquelhas@gmail.com>
2018-11-26 22:05:47 +0100

    :fire: removing extra code

--- src/main/java/edu/insightr/gildedrose/Inventory.java
+++ src/main/java/edu/insightr/gildedrose/Inventory.java
@@ -41,64 +41,10 @@ public class Inventory {
     public void updateQuality() {
         UpdateVisitor visitor = new UpdateVisitor();
         
-        int sum=0; 
         for(Item item : items) 
         { 
             item.accept(visitor); 
-        }         
-//        for (int i = 0; i < items.length; i++) {
-//            if (items[i].getName() != "Aged Brie"
-//                && items[i].getName() != "Backstage passes to a TAFKAL80ETC concert") {
-//                if (items[i].getQuality() > 0) {
-//                    if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
-//                        items[i].setQuality(items[i].getQuality() - 1);
-//                    }
-//                }
-//            } else {
-//                if (items[i].getQuality() < 50) {
-//                    items[i].setQuality(items[i].getQuality() + 1);
-//
-//                    if (items[i].getName() == "Backstage passes to a TAFKAL80ETC concert") {
-//                        if (items[i].getSellIn() < 11) {
-//                            if (items[i].getQuality() < 50) {
-//                                items[i].setQuality(items[i].getQuality() + 1);
-//                            }
-//                        }
-//
-//                        if (items[i].getSellIn() < 6) {
-//                            if (items[i].getQuality() < 50) {
-//                                items[i].setQuality(items[i].getQuality() + 1);
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//
-//            if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
-//                items[i].setSellIn(items[i].getSellIn() - 1);
-//                if (items[i].getName() == "Conjured Mana Cake") {
-//                    items[i].setQuality(items[i].getQuality() - 1);
-//                }
-//            }
-//
-//            if (items[i].getSellIn() < 0) {
-//                if (items[i].getName() != "Aged Brie") {
-//                    if (items[i].getName() != "Backstage passes to a TAFKAL80ETC concert") {
-//                        if (items[i].getQuality() > 0) {
-//                            if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
-//                                items[i].setQuality(items[i].getQuality() - 1);
-//                            }
-//                        }
-//                    } else {
-//                        items[i].setQuality(items[i].getQuality() - items[i].getQuality());
-//                    }
-//                } else {
-//                    if (items[i].getQuality() < 50) {
-//                        items[i].setQuality(items[i].getQuality() + 1);
-//                    }
-//                }
-//            }
-//        }
+        }
     }
 
     public static void main(String[] args) {
